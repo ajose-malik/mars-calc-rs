d26ba7c64604d46ea9f43f451845c6051d855f17
ajosemalik <ajose.malik@gmail.com>
2022-05-24 18:45:24 -0400

    implement calculate_weight_on_mars function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("weight on Mars: {}kg", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+(weight / 9.81) * 3.711
 }
